9fdd0693addb0718892270a2d32824b7e0595b3c
dwivedianshika583-source <dwivedianshika583@gmail.com>
2026-09-14 20:48:41 +0530

    Fix component import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Education from "./components/Education";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Skills from "./components/skills";
+import Projects from "./components/projects";
+import Education from "./components/education";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
 function App() {
   return (
     <>
